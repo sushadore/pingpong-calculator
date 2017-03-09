b05a95133f5873de5502bae0ebdbf84c524ccbfc
Susha Dore <susha.dore@gmail.com>
2017-03-09 11:33:50 -0800

    change multiples of 3 to ping fulfilling the second spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 var numbersArray = [];
 var numberList = function(number) {
   for (currentNumber = 1; currentNumber <= number; currentNumber +=1) {
-    numbersArray.push(currentNumber);
+    if (currentNumber % 3 === 0) {
+      numbersArray.push("ping");
+    } else {
+      numbersArray.push(currentNumber);
+    }
   }
-
 }
 
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    //$("#output").hide();
-
     var finalNumber = parseInt($("#number").val());
-    
     var result = numberList(finalNumber);
-
     var finalList = numbersArray.map(function(number) {
     $("ul").append("<li>" + number + "</li>");
     });
+
+    $("button#play").hide();
+    $("button#play-again").show();
 
   });
 });
