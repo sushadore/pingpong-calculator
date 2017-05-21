3acb6db6128f874530cb6278eab3a21f5c52e413
Susha Dore <susha.dore@gmail.com>
2017-05-21 15:20:32 -0700

    add clean and build function to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
+var del = require("del")
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
-var uglify = require('gulp-uglify');
+var uglify = require("gulp-uglify");
+var utilities = require("gulp-util")
 
+var buildProduction = utilities.env.production;
 
 gulp.task('concatInterface', function() {
   return gulp.src(['./js/*-interface.js'])
@@ -22,4 +25,16 @@ gulp.task("minifyScripts", ["jsBrowserify"], function(){
   return gulp.src("./build/js/app.js")
     .pipe(uglify())
     .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function(){
+  return del(["build", "tmp"]);
+})
+
+gulp.task("build", ["clean"], function(){
+  if(buildProduction) {
+    gulp.start("minifyScripts");
+  } else {
+    gulp.start("jsBrowserify");
+  }
 });
